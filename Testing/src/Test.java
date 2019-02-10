c2245f6a45ad74f055fb02cde9b5f0e84a6ce68e
MaramHasan <47158050+MaramHasan@users.noreply.github.com>
2019-02-10 20:50:50 +0200

    Try

---
+++ Testing/src/Test.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+import java. awt.*;
+
+public class Test {
+	public static void main(String[] args) {
+		int x =5 ;
+		int y=9;
+		int z =x+y ;
+		System.out.print(z);
+
+		
+		
+	}
+}
